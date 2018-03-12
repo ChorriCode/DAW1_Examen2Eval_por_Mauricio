1209a422d289846ab43bf6bb5f31406e851e856a
JavierHS <JavierHS@DESKTOP-RQ3V18B>
2018-03-12 13:18:15 +0000

    Mejorando la visualizacion del metodo mostrarAlumnosYNotas

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -9,11 +9,14 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		
 		String[] listadoAsignaturas = Datos.leerFicheroTxtConAsignaturas("ficheros/asignaturas.txt");
+		
 		HashMap<String, ArrayList<String>> listadoAlumnosNotas = Datos.leerFicheroAlumnosNotas("ficheros/notasAlumnos.txt");
 		Listado.mostrarAlumnosYNotas(listadoAsignaturas, listadoAlumnosNotas);
 		
 		Listado.notaMediaPorAlumno(listadoAlumnosNotas);
+		Listado.notaMediaPorAsignatura(listadoAsignaturas,listadoAlumnosNotas);
 	}
 
 }
--- src/vista/Listado.java
+++ src/vista/Listado.java
@@ -7,36 +7,51 @@ import java.util.Map.Entry;
 public class Listado {
 
 	public static void mostrarAlumnosYNotas(String[] listadoAsignaturas, HashMap<String,ArrayList<String>> listadoAlumnosNotas) {
-		
-		System.out.print("\t\t");
+		int indiceAsig;
+		System.out.print("\t\t\t");
 		for (int indiceAsignatura = 0; indiceAsignatura < listadoAsignaturas.length; indiceAsignatura++) {
 			System.out.print("\t" + listadoAsignaturas[indiceAsignatura]);
+			
 		}
-		System.out.println();
+		//System.out.println();
 		for (Entry<String, ArrayList<String>> listadoNotas : listadoAlumnosNotas.entrySet()) {
-			System.out.println("\n" + listadoNotas.getKey());
-			System.out.print("\t\t");
+			indiceAsig = 0;
+			System.out.print("\n" + listadoNotas.getKey());
+			System.out.print("\t");
 			for (String unaNotaDeUnaAsignatura : listadoNotas.getValue()) {
-				System.out.print("\t" + unaNotaDeUnaAsignatura);
+				
+				System.out.print(listadoAsignaturas[indiceAsig].length() > 6 ? "\t\t" + unaNotaDeUnaAsignatura : "\t" + unaNotaDeUnaAsignatura);
 			}
 
 		}
 	}
 	
 	public static void notaMediaPorAlumno(HashMap<String,ArrayList<String>> listadoAlumnosNotas) {
-		
+		System.out.println("\n*************** NOTAS MEDIA POR ALUMNOS **************");
 		for ( Entry<String, ArrayList<String>> alumnosNotas : listadoAlumnosNotas.entrySet()) {
 			float notaMedia = 0f;
 			float sumaNotas = 0;
 			for (int indiceNotas = 0; indiceNotas < alumnosNotas.getValue().size(); indiceNotas++) {
-
-				sumaNotas += Float.parseFloat(alumnosNotas.getValue().get(indiceNotas));
-				
+				sumaNotas += Float.parseFloat(alumnosNotas.getValue().get(indiceNotas));			
 			}
 			System.out.println();
-			System.out.println(sumaNotas);
 			notaMedia = sumaNotas/alumnosNotas.getValue().size();
-			System.out.println(notaMedia);
+			System.out.print("Nota Media del Alumno " + alumnosNotas.getKey() + " es: \t" +  notaMedia);
+		}
+	}
+	
+	public static void notaMediaPorAsignatura(String[] listadoAsignaturas, HashMap<String,ArrayList<String>> listadoAlumnosNotas) {
+		System.out.println("\n*************** NOTAS MEDIA POR ASIGNATURA **************");
+		for (int indiceNotas = 0; indiceNotas < listadoAsignaturas.length; indiceNotas++) {
+			float notaMedia = 0f;
+			float sumaNotas = 0;
+			for ( Entry<String, ArrayList<String>> alumnosNotas : listadoAlumnosNotas.entrySet()) {
+				
+				sumaNotas += Float.parseFloat(alumnosNotas.getValue().get(indiceNotas));			
+			}
+			System.out.println();
+			notaMedia = sumaNotas/listadoAlumnosNotas.size();
+			System.out.print("Nota Media de la Asignatura " + listadoAsignaturas[indiceNotas] + " es:    \t" +  notaMedia);
 		}
 	}
 }
